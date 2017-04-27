d87306b1427fc0acf87386cfc2eebbc18ed14600
Anastasia Kosogorova <kosogorova50@gmail.com>
2017-04-27 07:57:58 +0400

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,21 @@
+print("Calculator has started")
+while True:
+    a = float(input("Enter first number "))
+    b = float(input("Enter second number "))
+    chooseop=1
+    while (chooseop == 1) | (chooseop == 2) | (chooseop == 3) | (chooseop == 4):
+        chooseop = int(input("Enter 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division "))
+        print(chooseop)
+        if chooseop == 1:
+            print(a+b)
+        elif chooseop == 2:
+            print(a-b)
+        elif chooseop == 3:
+            print(a*b)
+        elif chooseop == 4:
+            if b == 0:
+                print("Can't divide by zero")
+            else:
+                print(a/b)
+        elif (chooseop != 1) & (chooseop != 2) & (chooseop != 3) & (chooseop != 4):
+            print("Invalid operation number")
