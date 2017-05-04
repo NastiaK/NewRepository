ccca85b34c2457a1314bf0f5edfb133f3a35b539
Anastasia Kosogorova <kosogorova50@gmail.com>
2017-05-04 08:13:03 +0400

    Add files via upload

---
+++ lab3oop.py
@@ -0,0 +1,27 @@
+
+class Polynom:
+    def __init__(self, string):
+        self.string=string
+
+    def formstring(self):
+        polynums = self.string.split(" ")
+        return polynums
+
+    def calculate(self, polynums):
+        sum=0
+        i=0
+        while i < len(polynums):
+            sum += (1 / int(polynums[i]))
+            i +=1
+        return sum
+
+
+def main():
+    string = str(input("Enter numbers for polynom divided by whitespaces: "))
+    poly = Polynom(string)
+    polynums = poly.formstring()
+    sum=poly.calculate(polynums)
+    print("Answer is ", sum)
+
+if __name__ == "__main__":
+    main()
