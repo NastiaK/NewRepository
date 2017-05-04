873f8a620ccb390127da5d33bf458b22383f3b4c
Anastasia Kosogorova <kosogorova50@gmail.com>
2017-05-04 08:12:00 +0400

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,9 @@
+number = float(input("Enter number you want to find square root for "))
+ans = 1.0
+prevans=0
+
+while (ans ** 2 != number) & (ans ** 2 != prevans):
+    prevans=ans
+    ans = 0.5 * (ans + number / ans)
+
+print("Answer is ", ans)
