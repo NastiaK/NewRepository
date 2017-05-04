bce6ac9c8e54ba3712ad0b63fa59e380953c09bf
Anastasia Kosogorova <kosogorova50@gmail.com>
2017-05-04 08:12:38 +0400

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,9 @@
+string = str(input("Enter numbers for polynom divided by whitespaces: "))
+polynums = string.split(" ")
+sum = 0
+i = 0
+
+while i<len(polynums):
+    sum += (1 / int(polynums[i]))
+    i +=1
+print("Answer is ", sum)
