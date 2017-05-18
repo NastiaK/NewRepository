d1907fc2e159dc1831071d7c79e20bbfb47fb822
Anastasia Kosogorova <kosogorova50@gmail.com>
2017-05-18 11:56:06 +0400

    Update lab3oop.py

--- lab3oop.py
+++ lab3oop.py
@@ -4,23 +4,31 @@ class Polynom:
         self.string=string
 
     def formstring(self):
-        polynums = self.string.split(" ")
+        polynums = self.string.split("--poly=")
+        polynums = polynums[1].split(";")
         return polynums
 
     def calculate(self, polynums):
-        sum=0
-        i=0
+        sum = 0
+        i = 0
+        error = 0
         while i < len(polynums):
-            sum += (1 / int(polynums[i]))
-            i +=1
+            try:
+                sum += (1 / int(polynums[i]))
+                i += 1
+            except ValueError:
+                error = 1
+                i += 1
+        if error == 1:
+            print("Warning: invalid values are ignored\n")
         return sum
 
 
 def main():
-    string = str(input("Enter numbers for polynom divided by whitespaces: "))
+    string = str(input("Awaiting command: "))
     poly = Polynom(string)
     polynums = poly.formstring()
-    sum=poly.calculate(polynums)
+    sum = poly.calculate(polynums)
     print("Answer is ", sum)
 
     
